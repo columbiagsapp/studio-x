e209d512b8fdac6e3ea99b67678628022bc1c2ce
Troy Conrad Therrien <troyth@gmail.com>
2012-08-20 16:17:02 -0400

    Added truncator.js and studio-x.js to truncate .caption

--- Tumblr/assets/studio-x.js
+++ Tumblr/assets/studio-x.js
@@ -1,3 +1,9 @@
 $(document).ready(function () {
+	var MAX_CAPTION_LENGTH = 400;
+
+
+	$(function() {
+      $('body:not(.permalink-page) .caption').truncate({max_length: MAX_CAPTION_LENGTH});
+    });
 
 });
